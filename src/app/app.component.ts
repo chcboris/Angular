8eea7a3c15986c9e48679749cefd78d9e7dd21f0
christian.moreira <christian.moreira@tre-rj.jus.br>
2023-03-12 15:31:10 -0300

    exemplo de acesso serviços

---
+++ src/app/app.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { ServidorService } from './servidor.service';
+import { Visitante } from './visitante';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'AngularService';
+  valor: String | undefined;
+  visitante?: Visitante;
+  i?: Number;
+
+  constructor(private srv: ServidorService) { }
+
+
+  visualizarTXT(){
+
+      this.srv.obter().subscribe({
+        next: (res)=> {
+          this.valor = res;
+          console.log(this.valor);             
+        },
+        error: (e)=> {
+          console.error(e);
+        }
+      });
+  }
+
+  ////////////////////////////////////////////////////////////////////// 
+
+  clicaNum(){
+    let valore: Number;
+    this.srv.getTesteNumero().subscribe(vlr => {
+      valore = vlr;
+      console.log(valore);   
+    }); 
+  }
+
+  servicoSimples(){
+
+    this.srv.testeVisitanteSimples().subscribe({
+      next: (res)=> {
+        this.visitante = res;
+        console.table(this.visitante);             
+      },
+      error: (e)=> {
+        console.error(e);
+      }
+    });
+}
+
+consultaPorId(ind: string){
+
+  this.srv.consultarPorId(ind).subscribe({
+    next: (res)=> {
+      this.visitante = res;
+      console.table(this.visitante);             
+    },
+    error: (e)=> {
+      console.error(e);
+    }
+  });
+}
+
+}
---
+++ src/app/servidor.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { Visitante } from './visitante';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServidorService {
+
+  url = 'http://localhost:4200/api/';
+
+  constructor(private http: HttpClient) { }
+
+  getTesteNumero(): Observable<Number> {
+      
+    return this.http.get<Number>(this.url + 'acessoVisitante/numero')
+      .pipe(
+        retry(2),
+        catchError(this.handleError))
+  }
+  
+  // Manipulação de erros
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Erro ocorreu no lado do client
+      errorMessage = error.error.message;
+    } else {
+      // Erro ocorreu no lado do servidor
+      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  };
+
+  //acionando um get retornando um jason
+  testeVisitanteSimples(): Observable<Visitante> {
+    console.log(this.url + `acessoVisitante/testarVisitante`);
+    return this.http.get<Visitante>(this.url + `acessoVisitante/testarVisitante`);
+  }
+
+  // acionando um get retornando um jason por id
+  consultarPorId(codigo: string): Observable<Visitante> {
+    return this.http.get<Visitante>(this.url + '/testarObterVisitante/' + codigo);
+  }
+
+/////////////////////////////////////////////// 
+  obter(): Observable<String> {
+    console.log(this.url + `acessoVisitante/texto`);
+   return this.http.get<String>(this.url + `acessoVisitante/texto`);
+}
+
+
+
+
+
+}
+
+
+/*
+ obterDiploma(impressaoId:number): Observable<Blob> {
+      console.log(Constantes.endpoint + `impressaoDiploma/obterDiploma/${impressaoId}`);
+     return this.http.get<Blob>(Constantes.endpoint + `impressaoDiploma/obterDiploma/${impressaoId}`,  { responseType: 'blob' as 'json' });
+  }
+
+*/
